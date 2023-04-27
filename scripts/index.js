c7e39e09c7628576ffbff7624d4a1b83e6df4ab7
Justin Muirhead <justin.muirhead92@gmail.com>
2023-04-26 23:21:17 -0400

    Project4Stage1_v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-initialCards = [
+const initialCards = [
    {name: "Boxed Water Is Better",
     link: "https://images.unsplash.com/photo-1570654621852-9dd25b76b38d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" },
     {name: "Andrea De Santis",
@@ -11,4 +11,4 @@ initialCards = [
         link: "https://plus.unsplash.com/premium_photo-1674898520828-87fbb9cc428e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
         {name: "La Petite Ceinture",
          link: "https://images.unsplash.com/photo-1556905200-279565513a2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
-]
+];
