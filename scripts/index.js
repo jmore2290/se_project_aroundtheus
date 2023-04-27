04b31123f6e46e6397666909981de62303542a08
Justin Muirhead <justin.muirhead92@gmail.com>
2023-04-26 23:01:47 -0400

    Project4Stage1_v1

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+initialCards = [
+   {name: "Boxed Water Is Better",
+    link: "https://images.unsplash.com/photo-1570654621852-9dd25b76b38d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" },
+    {name: "Andrea De Santis",
+     link: "https://images.unsplash.com/photo-1682282462523-996afa93708b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"},
+     {name: "Josh Hild",
+      link:"https://images.unsplash.com/photo-1681958757405-926494358753?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"},
+      {name: "Budapest, Hongrie",
+       link:"https://images.unsplash.com/photo-1549877452-9c387954fbc2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
+       {name: "Mumbia, India",
+        link: "https://plus.unsplash.com/premium_photo-1674898520828-87fbb9cc428e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"},
+        {name: "La Petite Ceinture",
+         link: "https://images.unsplash.com/photo-1556905200-279565513a2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+]
