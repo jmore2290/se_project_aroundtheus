a95454fda3455d8120357298b1585bd56d8eca0b
Justin Muirhead <justin.muirhead92@gmail.com>
2023-05-05 23:22:44 -0400

    sprint4_stage3_v1

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,18 @@ const initialCards = [
         {name: "La Petite Ceinture",
          link: "https://images.unsplash.com/photo-1556905200-279565513a2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
 ];
+
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#profile-modal-close");
+
+
+profileEditButton.addEventListener("click", () => {
+    profileEditModal.classList.add("modal__opened");
+
+});
+
+modalCloseButton.addEventListener("click", () =>{
+    profileEditModal.classList.remove("modal__opened");
+});
