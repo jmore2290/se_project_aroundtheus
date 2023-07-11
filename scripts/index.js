cfeb44d36bade3fee9860571cdc4e97dc0e43431
Justin Muirhead <justin.muirhead92@gmail.com>
2023-07-10 23:21:01 -0400

    Project4_Stage4_v1

--- scripts/index.js
+++ scripts/index.js
@@ -17,13 +17,61 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = document.querySelector("#profile-modal-close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector("#profile-description-input");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".gallery__list");
 
+/*    FUNCTIONS
+*/
+function closePopup(){
+    profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData){
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImageEl = cardElement.querySelector(".card__image");
+    const cardTitleEl = cardElement.querySelector(".card__title");
+    cardImageEl.setAttribute("alt", cardData.name);
+    cardImageEl.setAttribute("src",cardData.link);
+    // set the path of the image to the link field of the object
+    //set the image alt text to the name field of the object
+    //set the card title to the name field of the object.  too
+    cardTitleEl.textContent = cardData.name;
+    return cardElement;
+}
+
+/*   EVENT HANDLERS
+*/
+
+function handleProfileEditSubmit(e){
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+}
+
+
+
+
+/*EVENT LISTENERS
+*/
 
 profileEditButton.addEventListener("click", () => {
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal__opened");
 
 });
 
-modalCloseButton.addEventListener("click", () =>{
-    profileEditModal.classList.remove("modal__opened");
-});
+modalCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) =>{
+    const cardElement = getCardElement(cardData);
+    cardListEl.prepend(cardElement);
+})
